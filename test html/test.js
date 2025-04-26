628fcb8e3997b4331ba06503903204f619257432
Adham Ahmed Abbas <adhamshiketa9@gmail.com>
2025-04-26 11:59:51 +0300

    Add files via upload

---
+++ test html/test.js
@@ -0,0 +1,6 @@
+console.log("Adhom");
+console.error("damn");
+console.log("Adham %cAhmed Abbas","color: red ; font-size : 20px");
+console.log("Adham %cAhmed %cAbbas","color: red ; font-size : 20px","color: blue ; font-size : 20px");
+var myname = "adham";
+console.log(`hello ${myname}`);
